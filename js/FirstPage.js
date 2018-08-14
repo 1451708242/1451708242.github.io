307bf1a41149afc9648b61c7e12eb8bd6417b24c
1451708242 <1451708242@qq.com>
2018-08-14 11:10:52 +0800

    111

---
+++ js/FirstPage.js
@@ -0,0 +1,48 @@
+document.getElementById("chaXun").onclick = function () {
+    document.getElementById("ind").style.display = "none";
+    document.getElementById("check2").style.display = "none";
+    document.getElementById("check3").style.display = "none";
+    document.getElementById("check4").style.display = "none";
+    document.getElementById("check5").style.display = "none";
+    document.getElementById("check").style.display = "block";
+}
+document.getElementById("click1").onclick = function () {
+    document.getElementById("ind").style.display = "block";
+    document.getElementById("check2").style.display = "none";
+    document.getElementById("check3").style.display = "none";
+    document.getElementById("check4").style.display = "none";
+    document.getElementById("check5").style.display = "none";
+    document.getElementById("check").style.display = "none";
+}
+document.getElementById("click2").onclick = function () {
+    document.getElementById("ind").style.display = "none";
+    document.getElementById("check").style.display = "none";
+    document.getElementById("check2").style.display = "block";
+    document.getElementById("check3").style.display = "none";
+    document.getElementById("check4").style.display = "none";
+    document.getElementById("check5").style.display = "none";
+}
+document.getElementById("click3").onclick = function () {
+    document.getElementById("ind").style.display = "none";
+    document.getElementById("check").style.display = "none";
+    document.getElementById("check2").style.display = "none";
+    document.getElementById("check3").style.display = "block";
+    document.getElementById("check4").style.display = "none";
+    document.getElementById("check5").style.display = "none";
+}
+document.getElementById("click4").onclick = function () {
+    document.getElementById("ind").style.display = "none";
+    document.getElementById("check").style.display = "none";
+    document.getElementById("check2").style.display = "none";
+    document.getElementById("check3").style.display = "none";
+    document.getElementById("check4").style.display = "block";
+    document.getElementById("check5").style.display = "none";
+}
+document.getElementById("click5").onclick = function () {
+    document.getElementById("ind").style.display = "none";
+    document.getElementById("check").style.display = "none";
+    document.getElementById("check2").style.display = "none";
+    document.getElementById("check3").style.display = "none";
+    document.getElementById("check4").style.display = "none";
+    document.getElementById("check5").style.display = "block";
+}
---
+++ js/admin.js
@@ -0,0 +1,24 @@
+document.getElementById("block1").onclick = function () {
+    document.getElementById("Demo3-1").style.display = "block";
+    document.getElementById("Demo3-2").style.display = "none";
+    document.getElementById("Demo3-3").style.display = "none";
+    document.getElementById("Demo3-4").style.display = "none";
+}
+document.getElementById("block2").onclick = function () {
+    document.getElementById("Demo3-1").style.display = "none";
+    document.getElementById("Demo3-2").style.display = "block";
+    document.getElementById("Demo3-3").style.display = "none";
+    document.getElementById("Demo3-4").style.display = "none";
+}
+document.getElementById("block3").onclick = function () {
+    document.getElementById("Demo3-1").style.display = "none";
+    document.getElementById("Demo3-2").style.display = "none";
+    document.getElementById("Demo3-3").style.display = "block";
+    document.getElementById("Demo3-4").style.display = "none";
+}
+document.getElementById("block4").onclick = function () {
+    document.getElementById("Demo3-1").style.display = "none";
+    document.getElementById("Demo3-2").style.display = "none";
+    document.getElementById("Demo3-3").style.display = "none";
+    document.getElementById("Demo3-4").style.display = "block";
+}
